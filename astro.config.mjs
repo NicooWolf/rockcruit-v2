7226bb76c82d74c2200082be6f0988ae9dbf1877
Nicolas Wolf <nico.wolf@outlook.com>
2026-08-06 17:21:11 -0300

    set astro config to match gh pages url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://www.rockcruit.com",
+  site: "https://rockcruit-v2.github.io",
   integrations: [sitemap()],
 });
